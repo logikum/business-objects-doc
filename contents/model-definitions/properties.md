671c1e2c76a196261e20aee11406e95729b73cdb
Mármarosi József <marmarosi@logikum.hu>
2015-05-24 18:37:47 +0200

    Adding reference links generation.

--- contents/model-definitions/properties.md
+++ contents/model-definitions/properties.md
@@ -8,3 +8,15 @@ definition looks like that:
 ```
 var property = new Property(name, type, flags, getter, setter);
 ```
+
+Arguments `name` and `type` are required. Argument `name` is a string, and it defines
+the name of the property. Argument `type` either a [DataType] member or another model's type:
+
+```
+var price = new Property('price', DataType.Decimal);
+var customer = new Property('customer', Customer);
+```
+
+The following data types are supported out of the box: [Boolean], [Text], [Email],
+[Integer], [Decimal], [Enum] and [DateTime]. Custom data types can be created as well.
+See [Data types](data-types) page for details.
---
+++ gen-refs.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var referenceDir = '../business-objects/doc-baseline';
+var referenceFile = 'references.txt';
+var count = 0;
+var text = '[~]: http://logikum.github.io/business-objects\r\n';
+
+// Read directory items.
+var items = fs.readdirSync(path.join(process.cwd(), referenceDir));
+
+items.forEach(function (item) {
+
+  // Get full path of item.
+  var itemPath = path.join(referenceDir, item);
+  // Get item info.
+  var stats = fs.statSync(itemPath);
+
+  if (stats.isFile() && path.extname(item) === '.html' &&
+      path.extname(path.basename(item, '.html')) !== '.js') {
+
+    var name = path.basename(item, '.html');
+    if (name.indexOf('.') >= 0)
+      name = name.substr(name.lastIndexOf('.') + 1);
+
+    text += '[' + name + ']: ~/' + item + '\r\n';
+    count++;
+  }
+});
+
+fs.writeFile(path.join(process.cwd(), referenceFile), text);
+
+console.log('Count of references: ' + count);
